5d27a194628378c463cbd4c2d62ed5923bd280ba
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-11-06 14:07:29 +0100

    Upgrade to tasty-query 0.3.1.

--- tasty-mima/src/test/scala/tastymima/ElementaryLoadingSuite.scala
+++ tasty-mima/src/test/scala/tastymima/ElementaryLoadingSuite.scala
@@ -9,23 +9,23 @@ class ElementaryLoadingSuite extends munit.FunSuite:
     val ctx = tastyquery.Contexts.init(TestClasspaths.testLibV1Classpath)
     given Context = ctx
 
-    val ClassOnlyInV1Class = ctx.findSymbolFromRoot(termName("testlib") :: typeName("ClassOnlyInV1") :: Nil).asClass
+    val ClassOnlyInV1Class = ctx.findTopLevelClass("testlib.ClassOnlyInV1")
 
     val testLibSymbols = ctx.findSymbolsByClasspathEntry(TestClasspaths.testLibV1Entry)
     assert(testLibSymbols.exists(_ == ClassOnlyInV1Class))
 
-    intercept[MemberNotFoundException](ctx.findSymbolFromRoot(termName("testlib") :: typeName("ClassOnlyInV2") :: Nil))
+    intercept[MemberNotFoundException](ctx.findTopLevelClass("testlib.ClassOnlyInV2"))
   }
 
   test("ClassOnlyInV2") {
     val ctx = tastyquery.Contexts.init(TestClasspaths.testLibV2Classpath)
     given Context = ctx
 
-    val ClassOnlyInV2Class = ctx.findSymbolFromRoot(termName("testlib") :: typeName("ClassOnlyInV2") :: Nil).asClass
+    val ClassOnlyInV2Class = ctx.findTopLevelClass("testlib.ClassOnlyInV2")
 
     val testLibSymbols = ctx.findSymbolsByClasspathEntry(TestClasspaths.testLibV2Entry)
     assert(testLibSymbols.exists(_ == ClassOnlyInV2Class))
 
-    intercept[MemberNotFoundException](ctx.findSymbolFromRoot(termName("testlib") :: typeName("ClassOnlyInV1") :: Nil))
+    intercept[MemberNotFoundException](ctx.findTopLevelClass("testlib.ClassOnlyInV1"))
   }
 end ElementaryLoadingSuite
